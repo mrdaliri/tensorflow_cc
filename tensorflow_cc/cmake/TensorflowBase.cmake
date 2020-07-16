ac7e4fca9ea414049872c9bbc7c9d79b0a4c93d4
Mohammad-Reza <rmdaliri@gmail.com>
2020-07-15 23:58:19 -0600

    Edit tf_version_script.lds file before build to include protobuf

--- tensorflow_cc/cmake/TensorflowBase.cmake
+++ tensorflow_cc/cmake/TensorflowBase.cmake
@@ -10,7 +10,7 @@ ExternalProject_Add(
   SOURCE_DIR "tensorflow"
   BUILD_IN_SOURCE 1
   UPDATE_COMMAND ""
-  CONFIGURE_COMMAND ""
+  CONFIGURE_COMMAND awk -i inplace "/local:/{print \"    *protobuf*$<SEMICOLON>\"}1" tensorflow/tf_version_script.lds
   BUILD_COMMAND "${CMAKE_CURRENT_BINARY_DIR}/build_tensorflow.sh"
   INSTALL_COMMAND "${CMAKE_CURRENT_SOURCE_DIR}/cmake/copy_links.sh" bazel-bin
 )
